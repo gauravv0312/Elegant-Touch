730143191711dfc9d0edf3808489b76a06d241d3
Gaurav Verma <70060764+gauravv0312@users.noreply.github.com>
2021-08-08 11:48:50 +0530

    update

--- Elegant_Touch/app/src/main/java/com/example/elegant_touch/Dashboard/HomeFragment.java
+++ Elegant_Touch/app/src/main/java/com/example/elegant_touch/Dashboard/HomeFragment.java
@@ -8,15 +8,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.elegant_touch.R;
 import com.example.elegant_touch.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class HomeFragment extends Fragment {
     FragmentHomeBinding binding;
+    List<SlideModel> slideModels = new ArrayList<SlideModel>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
        binding = FragmentHomeBinding.inflate(inflater,container,false);
+        slideModels.add(new SlideModel(R.drawable.slider1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider3, ScaleTypes.FIT));
+        binding.imageSlider.setImageList(slideModels);
+
         return binding.getRoot();
+
     }
 }
