a5006339068b4bfa7b5feeeb5696b3a75fe25446
Gaurav Verma <70060764+gauravv0312@users.noreply.github.com>
2021-08-14 14:20:26 +0530

    update

--- Elegant_Touch/app/src/main/java/com/example/elegant_touch/Dashboard/DashboardActivity.java
+++ Elegant_Touch/app/src/main/java/com/example/elegant_touch/Dashboard/DashboardActivity.java
@@ -2,22 +2,26 @@ package com.example.elegant_touch.Dashboard;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.elegant_touch.R;
 import com.example.elegant_touch.databinding.ActivityDashboardBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
      ActivityDashboardBinding binding;
     List<SlideModel> slideModels = new ArrayList<SlideModel>();
     @Override
@@ -30,12 +34,48 @@ public class DashboardActivity extends AppCompatActivity {
         slideModels.add(new SlideModel(R.drawable.slider3, ScaleTypes.FIT));
         binding.imageSlider.setImageList(slideModels);
         setContentView(binding.getRoot());
-//        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new HomeFragment()).commit();
+        //        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new HomeFragment()).commit();
+
+
+
+//        drawer
+        navigationDrawer();
+
+    }
+
+    private void navigationDrawer() {
+        binding.navigationView.bringToFront();  // for interaction
+        binding.navigationView.setNavigationItemSelectedListener(this);
+        binding.navigationView.setCheckedItem(R.id.home);
+        binding.drawerIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.drawerLayout.isDrawerVisible(GravityCompat.START))
+                {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                else
+                {
+                    binding.drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        finishAffinity();
+        if (binding.drawerLayout.isDrawerVisible(GravityCompat.START))
+        {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+//        finishAffinity();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
