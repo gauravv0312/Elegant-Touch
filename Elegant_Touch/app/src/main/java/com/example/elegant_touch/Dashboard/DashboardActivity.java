93859aa520967bcbfd7be63793759afa015128a0
Gaurav Verma <70060764+gauravv0312@users.noreply.github.com>
2021-08-14 12:07:31 +0530

    update

--- Elegant_Touch/app/src/main/java/com/example/elegant_touch/Dashboard/DashboardActivity.java
+++ Elegant_Touch/app/src/main/java/com/example/elegant_touch/Dashboard/DashboardActivity.java
@@ -8,39 +8,29 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.elegant_touch.R;
 import com.example.elegant_touch.databinding.ActivityDashboardBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DashboardActivity extends AppCompatActivity {
      ActivityDashboardBinding binding;
+    List<SlideModel> slideModels = new ArrayList<SlideModel>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        slideModels.add(new SlideModel(R.drawable.slider1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.slider3, ScaleTypes.FIT));
+        binding.imageSlider.setImageList(slideModels);
         setContentView(binding.getRoot());
-        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new HomeFragment()).commit();
-       binding.bottomnavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-           @Override
-           public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-               Fragment temp=null;
-               switch (item.getItemId())
-               {
-                   case R.id.home: temp =new HomeFragment();
-                   break;
-                   case R.id.category: temp =new CategoryFragment();
-                   break;
-                   case R.id.order: temp =new OrderFragment();
-                   break;
-                   case R.id.profile: temp =new SettingFragment();
-                   break;
-
-               }
-               getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,temp).commit();
-               return true;
-           }
-       });
+//        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout,new HomeFragment()).commit();
     }
 
     @Override
