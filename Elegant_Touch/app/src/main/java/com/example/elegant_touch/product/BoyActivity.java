704d0af1e5eb562b4db8fe325f3b048bc54ca472
Gaurav Verma <70060764+gauravv0312@users.noreply.github.com>
2021-09-15 12:54:16 +0530

    update

---
+++ Elegant_Touch/app/src/main/java/com/example/elegant_touch/product/BoyActivity.java
@@ -0,0 +1,16 @@
+package com.example.elegant_touch.product;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.elegant_touch.R;
+
+public class BoyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boy);
+    }
+}
